d5d3ca90e733db45b1150c94e3a57adb6ff6fa5d
Mohamed Hemdan <hemdan@drjobs.ae>
2024-04-24 12:29:51 +0200

    logic update

--- src/components/Article/ArticleList.jsx
+++ src/components/Article/ArticleList.jsx
@@ -1,30 +1,26 @@
+// ArticleList.js
+
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { Container, Form, Row } from 'react-bootstrap';
 import Spinner from '../loader/Spinner';
 import Header from './Header';
 import Article from './Article';
+import { fetchArticles } from '../../../utilities/api'; // Import the utility function
 
 const ArticleList = () => {
-  const API_KEY = 'O7gGsXkWC45redFL7BIsrk5iLrCEqqHw';
   const [articles, setArticles] = useState([]);
-  const [period, setPeriod] = useState(1); // Removed term state
+  const [period, setPeriod] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchArticlesData = async () => {
-      try {
-        const response = await axios.get(`https://api.nytimes.com/svc/mostpopular/v2/viewed/${period}.json?api-key=${API_KEY}`);
-        setArticles(response.data.results);
-        setIsLoading(false);
-      } catch (error) {
-        console.error("Error fetching articles:", error);
-        setIsLoading(false);
-      }
+      const data = await fetchArticles(period); // Call the utility function
+      setArticles(data);
+      setIsLoading(false);
     };
 
     fetchArticlesData();
-  }, [period]); // Removed term dependency
+  }, [period]);
 
   const handleChange = (e) => {
     setPeriod(parseInt(e.target.value));
@@ -44,10 +40,10 @@ const ArticleList = () => {
           </Form.Select>
         </div>
         <Container>
-        <Row>
-          {articles.map(article => (
-            <Article key={article.id} article={article} />
-          ))}
+          <Row>
+            {articles.map(article => (
+              <Article key={article.id} article={article} />
+            ))}
           </Row>
         </Container>
       </Container>
--- utilities/api.js
+++ utilities/api.js
@@ -1,16 +1,13 @@
 import axios from 'axios';
 
 const API_KEY = 'O7gGsXkWC45redFL7BIsrk5iLrCEqqHw';
-const BASE_URL = 'https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=';
 
-export const fetchArticlesData = async (term,period) => {
+export const fetchArticles = async (period) => {
   try {
-      const response = await axios.get(
-          `https://api.nytimes.com/svc/mostpopular/v2/${term}/${period}.json?api-key=${API_KEY}`
-      );
-      //console.log(response)
-      return response?.data?.results
+    const response = await axios.get(`https://api.nytimes.com/svc/mostpopular/v2/viewed/${period}.json?api-key=${API_KEY}`);
+    return response.data.results;
   } catch (error) {
-      return error
+    console.error("Error fetching articles:", error);
+    return [];
   }
 };
